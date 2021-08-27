38988707401898c10c18b584e008642179df8d8d
Manoel <manoelfangmo@gmail.com>
2021-08-27 14:30:18 -0400

    git hub added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,35 +1,39 @@
-import React from 'react'
-import './topbar.scss'
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import MailIcon from '@material-ui/icons/Mail';
-export default function Topbar({menuOpen, setMenuOpen}) {
-    return (
-        <div className={"topbar " + (menuOpen && "active")}>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo" >relentless.</a>
-                    <div className="itemContainer">
-                        <LinkedInIcon className="icon" />
-                        <span><a href="https://www.linkedin.com/in/manoel-fangmo/">Linkedin</a></span>
-                    </div>
-                    <div className="itemContainer">
-                        <MailIcon className="icon" />
-                        <span>manoelfangmo@gmail.com</span>
-                    </div>
-
-                </div>
-        
-                <div className="right">
-                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)} >
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                    
-                </div>
-
-            </div>
-            
+import React from "react";
+import "./topbar.scss";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            relentless.
+          </a>
+          <div className="itemContainer">
+            <LinkedInIcon className="icon" />
+            <span>
+              <a href="https://www.linkedin.com/in/manoel-fangmo/">Linkedin</a>
+            </span>
+          </div>
+          <div className="itemContainer">
+            <GitHubIcon className="icon" />
+            <span>
+              <a href="https://github.com/manoelfangmo?tab=repositories">
+                GitHub
+              </a>
+            </span>
+          </div>
         </div>
-    )
+
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
