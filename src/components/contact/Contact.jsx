c54a9e99dd64c9259db61241e7636147d6866b85
Manoel <manoelfangmo@gmail.com>
2021-08-27 13:43:39 -0400

    Clear Cell Game added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,13 +2,38 @@ import React from "react";
 import { useState } from "react";
 import "./contact.scss";
 import shake from "../../assets/shake.svg";
+import { send } from "emailjs-com";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
-  const handleSubmit = (e) => {
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    to_name: "Manoel",
+    message: "",
+    reply_to: "",
+  });
+
+  const onSubmit = (e) => {
     e.preventDefault();
     setMessage(true);
+    send(
+      "service_ox1rlp9",
+      "template_jhlbqc6",
+      toSend,
+      "user_nHthDlJJLpZN2zTxDtZ7x"
+    )
+      .then((response) => {
+        console.log("SUCCESS!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.log("FAILED...", err);
+      });
   };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -16,21 +41,31 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        {/* <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="Submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
-        </form> */}
-        <form
-          action="https://formsubmit.co/manoelfangmo@gmail.com"
-          method="POST"
-          onSubmit={handleSubmit}
-        >
-          <input type="text" name="name" placeholder="Name" required />
-          <input type="email" name="email" placeholder="Email" required />
-          <textarea name="messsage" placeholder="Message"></textarea>
-          <button type="submit">Send</button>
+        <form className="contact-form" onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="from_name"
+            placeholder="name"
+            value={toSend.from_name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="reply_to"
+            placeholder="email"
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+          <textarea
+            type="text"
+            name="message"
+            placeholder="message"
+            value={toSend.message}
+            onChange={handleChange}
+          />
+
+          <button type="submit">Submit</button>
+          {/* </button> */}
           {message && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
       </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,64 +1,79 @@
-import React from 'react'
-import './portfolio.scss'
-import { useState } from 'react';
-import comingSoon from "../../assets/comingSoon.jpg"
-import arrow from "../../assets/arrow2.png"
-
+import React from "react";
+import "./portfolio.scss";
+import { useState } from "react";
+import comingSoon from "../../assets/comingSoon.jpg";
+import arrow from "../../assets/arrow.png";
+import clearCellGame from "../../assets/clearCellGame.png";
 
 export default function Portfolio() {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const data = [
-        {
-            img: comingSoon,
-            title: "Personal Training Website",
-            skills: ["Javascript","HTML","SCSS","React"],
-        },
-        {
-            img: comingSoon,
-            title: "Mobile Application",
-            skills: ["Javascript","HTML","SCSS","React"],
-        },
-        {
-            img: comingSoon,
-            title: "Scheduler Application",
-            skills: ["Javascript","HTML","SCSS","React"],
-        },
-    ]
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      img: clearCellGame,
+      title: "Clear Cell Game",
+      skills: ["Javascript", "HTML", "SCSS", "React"],
+      link: "https://manoelfangmo.github.io/clearCellGame/",
+    },
+    {
+      img: comingSoon,
+      title: "Mobile Application",
+      skills: ["Javascript", "HTML", "SCSS", "React"],
+      link: "",
+    },
+    {
+      img: comingSoon,
+      title: "Scheduler Application",
+      skills: ["Javascript", "HTML", "SCSS", "React"],
+      link: "",
+    },
+  ];
 
-    const handleClick = (way) =>{
-        way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
-        setCurrentSlide(currentSlide<data.length-1 ? currentSlide+1 : 0);
-    }
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
 
-    return (
-        <div className="portfolio" id="portfolio">
+  return (
+    <div className="portfolio" id="portfolio">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <img src={d.img} alt="" />
 
+              <div className="description">
+                <h2>{d.title}</h2>
+                <h3>Skills Used</h3>
+                <ul>
+                  {d.skills.map((x) => (
+                    <li>{x}</li>
+                  ))}
+                </ul>
 
-            <div className="slider" style={{transform:`translateX(-${currentSlide*100}vw)`}}>
-                {data.map((d) => (
-                    <div className="container">
-                        <div className="item">
-                            
-                            <img src={d.img} alt="" />
-
-                            <div className="description">
-                                <h2>{d.title}</h2>
-                                <h3>Skills Used</h3>
-                                {d.skills.map((x) =>(
-                                <ul>
-                            
-                                    <li>{x}</li>
-                                </ul>
-                                ))}
-                                <button>Open</button>
-                            </div>
-                        </div>
-                    </div> ))
-                }
+                <button>
+                  <a href={d.link}>Open</a>
+                </button>
+              </div>
             </div>
-            <img src={arrow} alt="" className="arrow left" onClick={()=>handleClick("left")}/>
-            <img src={arrow} alt="" className="arrow right" onClick={()=>handleClick()} />
-            
-        </div> 
-    )
+          </div>
+        ))}
+      </div>
+      <img
+        src={arrow}
+        alt=""
+        className="arrow left"
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src={arrow}
+        alt=""
+        className="arrow right"
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
